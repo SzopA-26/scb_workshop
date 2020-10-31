2a3a75d485c47a3d5e2d837fe7b530c76c6260cc
Peranut Wattanakulsiri <peranut.w@ku.th>
2020-10-31 10:37:26 +0700

    Impl game fizzbuzz

--- app/fizzbuzz/index.js
+++ app/fizzbuzz/index.js
@@ -14,6 +14,24 @@
 
 function fizzBuzz(n) {
     const resp = []
+    for (let i = 1; i <= n; i++) {
+        let result
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            result = 'fizzbuzz'
+        } else if (i % 3 == 0) {
+            result = 'fizz'
+        } else if (i % 5 == 0) {
+            result = 'buzz'
+        } else {
+            result = i
+        }
+
+        resp.push({
+            num: i,
+            val: result
+        })
+    }
     //resp = [{num: x, val: y}]
     return resp
 }
